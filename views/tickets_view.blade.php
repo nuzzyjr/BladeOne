6f36cf5571ab32c5ea95a76b11bb7884c187e47c
nuzzyjr <nicolf15@icloud.com>
2022-01-14 16:03:19 +0000

    pok

--- views/tickets_view.blade.php
+++ views/tickets_view.blade.php
@@ -3,6 +3,22 @@
 
 @section ('content')
 
-    <p>tickets lol</p>
+
+    <?php
+
+        $conn = mysqli_connect("localhost","root","","phpcrud") or die ("Could not connect to database!");
+
+
+        $sql = "SELECT fname FROM people WHERE id='25'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "First name: " . $row["fname"]. "<br>";
+        }
+        }
+        $conn->close();
+    ?>
 
 @endsection
